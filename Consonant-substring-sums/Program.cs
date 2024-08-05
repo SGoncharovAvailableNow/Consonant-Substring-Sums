2ab7a7e29ed36aade8f31e6e937d6edbf464508b
Serg_Goncharov <s.goncharov@sh.dunice.net>
2024-08-05 11:14:02 +0300

    substring-sums method

--- Consonant-substring-sums/Program.cs
+++ Consonant-substring-sums/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using Consonant_substring_sums.SubstringSums;
+using System.Runtime.CompilerServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var substringSum = new SubstringSums();
+
+        //substringSum.ShowResult(substringSum.SubtringSum(substringSum._testStrings.First()));
+
+        foreach (var item in substringSum._testStrings)
+        {
+            substringSum.ShowResult(substringSum.SubtringSum(item));
+        }
+    }
+}
